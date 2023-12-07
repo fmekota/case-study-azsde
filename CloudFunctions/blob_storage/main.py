f57c087300cec3dd6799491911a7a26bec163951
Filip Mekota <filip@mekota.eu>
2023-12-07 12:34:28 +0100

    blob_storage fce implemented

---
+++ CloudFunctions/blob_storage/main.py
@@ -0,0 +1,123 @@
+import io
+import os
+import logging
+
+import requests
+import pandas as pd
+
+from google.cloud import bigquery
+
+def cleanup_download(csv_file):
+    '''
+    This function cleans up the downloaded CSV file.
+    Input: CSV file
+    Output: Cleaned up CSV file as pandas DataFrame
+    '''
+    # Read the data into a Pandas DataFrame
+    df = pd.read_csv(csv_file, on_bad_lines="skip")
+
+    # Clean the DataFrame
+    df.iloc[:, 0] = pd.to_numeric(df.iloc[:, 0], errors='coerce').fillna(0).astype(int)
+    
+    df.iloc[:, 1] = df.iloc[:, 1].astype(str)
+    
+    df.iloc[:, 2] = df.iloc[:, 2].astype(str).str.replace(',', '.').astype(float)
+
+    df.convert_dtypes()
+
+    # Log the data types and number of missing values
+    logging.info("Data type of column 0: %s, number of nans: %s", df.iloc[:, 0].dtype, df['bike_id'].isnull().sum())
+    logging.info("Data type of column 1: %s, number of nans: %s", df.iloc[:, 1].dtype, df['manufacturer'].isnull().sum())
+    logging.info("Data type of column 2: %s, number of nans: %s", df.iloc[:, 2].dtype, df['manufacturer'].isnull().sum())
+
+    return df
+
+def calculate_max_error(df, max_error_percent):
+    '''
+    This function calculates the maximum number of errors allowed to keep a specified percent of the data.
+    Input: Pandas DataFrame, maximum error percent
+    Output: Maximum number of errors allowed
+    '''
+    # Calculate number of records that can be errored out to keep specified percent of the data
+    num_records = len(df)
+    num_errors = round(num_records * (max_error_percent / 100))
+    logging.info("Number of records: %s", num_records)
+    logging.info("Number of allowed errors: %s", num_errors)
+
+    return num_errors
+
+
+def download_and_upload_to_bq(request):
+    '''
+    This function downloads a file from Azure Blob Storage and uploads it to BigQuery.
+    '''
+    # Initialize the BigQuery client
+    bq_client = bigquery.Client()
+
+    # Set the logging level
+    logging.basicConfig(level=logging.INFO)
+
+    # Azure Blob Storage SAS URL
+    sas_url = os.environ.get('AZURE_BLOB_SAS_URL')
+
+    # BigQuery dataset and table information
+    dataset_id = os.environ.get('BIGQUERY_DATASET_ID')
+    table_id = os.environ.get('BIGQUERY_TABLE_ID')
+
+    # Maximum error percent allowed
+    max_error_percent = float(os.environ.get('MAX_ERROR_PERCENT'))
+
+    try:
+        # Send an HTTP GET request to the Azure Blob Storage URL
+        response = requests.get(sas_url)
+        logging.info("Response status code: %s",  response.status_code)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Convert bytes response to a file-like object
+            data_file = io.BytesIO(response.content)
+
+            # Clean up the CSV file
+            cleaned_data = cleanup_download(data_file)
+
+            # Create a BigQuery dataset reference
+            dataset_ref = bq_client.dataset(dataset_id)
+
+            # Create a BigQuery table reference
+            table_ref = dataset_ref.table(table_id)
+
+            # Calculate the maximum number of errors allowed
+            num_errors = calculate_max_error(cleaned_data, max_error_percent)
+
+            # Configure the job to append data to the table
+            job_config = bigquery.LoadJobConfig(
+                source_format=bigquery.SourceFormat.CSV,
+                skip_leading_rows=1,  # Skip the header row
+                write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+                max_bad_records=num_errors,  # Number of errors allowed to keep specified percent of the data
+
+            )
+
+            # Load the data into the BigQuery table
+            load_job = bq_client.load_table_from_dataframe(cleaned_data, table_ref, job_config=job_config)
+            load_job.result()
+
+            # Check if the load job completed successfully
+            if load_job.state == 'DONE':
+                if load_job.errors:
+                    logging.error("Load job completed with errors: %s", load_job.errors)
+                    return "Load job completed with errors"
+                else:
+                    logging.info("Data successfully loaded into BigQuery table %s.%s", dataset_id, table_id)
+                    return f"Data loaded into BigQuery table {dataset_id}.{table_id}"
+            else:
+                logging.error("Load job did not complete successfully. Final state: %s", load_job.state)
+                return f"Load job did not complete successfully. Final state: {load_job.state}"
+
+        else:
+            logging.error("Failed to download file. Status code: %s", response.status_code)
+            return f"Failed to download file. Status code: {response.status_code}"
+
+    except Exception as e:
+        logging.error(e)
+        return str(e)
--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # case-study-azsde
+This codebase is a PoC for the case study assigned on 2023-12-01. Goal is to design and implement simple, yet functional pipeline within the context of GCP that will:
+
+'''
+Zadání: Z níže uvedených zdrojů dat, vytvořte datovou pipelinu, která jednou za hodinu stáhne data
+do vhodně navržené cloudové architektury včetně transformačních procesů. Ze zdroje 1 stáhne data,
+následně až tento proces skončí, tak ze zdroje 2 a 3 (paralelně) a nakonec ze zdroje 4.
+Zdroje dat:
+1. [zdroj 1] .alza soubor:
+• Stáhněte soubor z cloudového úložiště, do kterého se přihlaste pomocí tohoto sas_url:
+https://stalzadfcase.blob.core.windows.net/sales-data?sp=r&st=2023-11-
+30T10:39:13Z&se=2023-12-14T11:00:13Z&spr=https&sv=2022-11-
+02&sr=c&sig=HWRihJvJCStY45hh5XwjAHOgBJvjZNhQDVo0p2PCgqk%3D.
+• Výstup: Uložte data tak, aby byla použitelná pro jejich další analýzu.
+2. [zdroj 2] Devizový trh ČNB:
+• Stažení dat z devizového trhu ČNB v rozsahu od 20.12.2022 do 3.2.2023.
+• Výstup: Uložte data tak, aby byla použitelná pro jejich další analýzu.
+o Provedení transformací nad daty je na Vás.
+
+3. [zdroj 3] API s historickými údaji o počasí:
+• Napojte se na volně dostupné API s historickými údaji o počasí.
+o Např. https://www.visualcrossing.com/weather/weather-data-services/
+• Stáhněte data pro město Austin v Texasu, USA, proveďte Vámi navrhované transformace.
+• Výstup: Vhodně uložte historická data o počasí s ohledem na jejich další analytickou
+využitelnost.
+4. [zdroj 4] BigQuery dataset:
+
+• Napojte se na data z veřejně dostupného BigQuery Dataset_ID „bigquery-public-
+data.austin_bikeshare“. Definice zadání:
+
+a) Sestavte dotaz, který nám vrátí pro daný den, výchozí stanici, jaké kolo bylo
+využito (bike_id) a dobu strávenou na kole v hodinách. Zajímají nás jen elektrická
+kola. Stanice výchozí a cílová musí být totožná a zároveň aktivní.
+▪ Výstup: Tato data zpracujte a uložte tak, aby byla připravena pro další
+analýzy na základě níže definovaného požadavku "b)".
+
+b) Výše připravená data použijte a spojte se zdrojem [1], [2] a [3], aby finální výstup
+byl následující:
+▪ Předchozí data obohaťte a pro každý den dále zobrazte, jaké bylo počasí
+(průměrná teplota ve stupních Celsia) v analyzovaném městě, název kola
+(výrobce) pro daný „trip“ a cenu kola zobrazte v Kč na základě kurzů z
+ČNB.
+
+Požadavky:
+• Cloudová platforma: Zvolte vhodnou cloudovou platformu pro implementaci ETL/ELT
+procesů (AWS, Azure, GCP), s preferencí pro Google Cloud Platform (GCP).
+• Pipelines/ETL procesy: Vytvořte efektivní a škálovatelné ETL procesy pro stahování,
+transformaci a ukládání dat z každého z čtyř zdrojů.
+• Orchestrační pipelina: Navrhněte pipelinu, která bude spouštěčem jednotlivých událostí a
+bude časově nastavená.
+• Monitoring dat: Popište, jak budou data monitorována během ETL/ELT procesů, a jak budou
+řešeny případné chyby v průběhu zpracování dat. [bod navíc, jen se zamyslete a případně pár
+větami popište v přiložené výstupní dokumentaci]
+• Architektura: Navrhněte cloudovou architekturu pro ukládání dat, s důrazem na
+škálovatelnost, výkon a správné fungování ETL/ELT procesů.
+• Dokumentace: K case study očekáváme doprovod v podobě detailní dokumentace, která
+popisuje váš přístup, rozhodnutí při návrhu a implementaci, a také případné kompromisy.
+Uveďte relevantní technické detaily a vysvětlení, jak jste překonali případné obtíže.
+Souhrn k části č. 1: Cílem této první části case study je demonstrace vaší schopnosti efektivně
+zpracovávat data z různých zdrojů pomocí ETL/ELT procesů, s důrazem na bezpečnost, monitorování
+a navrhovanou cloudovou architekturu. Pro tuto část je také očekáván doprovod v podobě
+dokumentace.
+'''
