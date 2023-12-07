c0190ffca1f6506faf5d9e0e8621a996bd8e02e7
Filip Mekota <filip@mekota.eu>
2023-12-07 21:49:41 +0100

    workflow running

--- deploy.bat
+++ deploy.bat
@@ -8,6 +8,7 @@ REM Create a temporary copy of env.yaml
 copy C:\Users\Filip\Documents\case-study-azsde\case-study-azsde\CloudFunctions\blob_storage\env.yaml C:\Users\Filip\Documents\case-study-azsde\case-study-azsde\CloudFunctions\blob_storage\temp_env.yaml
 copy C:\Users\Filip\Documents\case-study-azsde\case-study-azsde\CloudFunctions\devizovy_trh\env.yaml C:\Users\Filip\Documents\case-study-azsde\case-study-azsde\CloudFunctions\devizovy_trh\temp_env.yaml
 copy C:\Users\Filip\Documents\case-study-azsde\case-study-azsde\CloudFunctions\weather_report\env.yaml C:\Users\Filip\Documents\case-study-azsde\case-study-azsde\CloudFunctions\weather_report\temp_env.yaml
+copy C:\Users\Filip\Documents\case-study-azsde\case-study-azsde\CloudFunctions\public_trips\env.yaml C:\Users\Filip\Documents\case-study-azsde\case-study-azsde\CloudFunctions\public_trips\temp_env.yaml
 
 REM Add the variables to the temporary env.yaml files
 for /r ./CloudFunctions/blob_storage %%f in (temp_env.yaml) do (
@@ -23,6 +24,12 @@ for /r ./CloudFunctions/devizovy_trh %%f in (temp_env.yaml) do (
 )
 
 for /r ./CloudFunctions/weather_report %%f in (temp_env.yaml) do (
+    echo.>>"%%f"
+    echo START_DATE: "%start_date%">>"%%f"
+    echo END_DATE: "%end_date%">>"%%f"
+)
+
+for /r ./CloudFunctions/public_trips %%f in (temp_env.yaml) do (
     echo.>>"%%f"
     echo START_DATE: "%start_date%">>"%%f"
     echo END_DATE: "%end_date%">>"%%f"
@@ -44,7 +51,7 @@ echo "Function blob_storage deployed successfully"
 echo "Deploying function: devizovy_trh"
 call gcloud functions deploy devizovy_trh ^
     --runtime python310 ^
-    --trigger-topic case_study_default_topic ^
+    --trigger-http ^
     --allow-unauthenticated ^
     --source ./CloudFunctions/devizovy_trh/ ^
     --env-vars-file CloudFunctions/devizovy_trh/temp_env.yaml ^
@@ -56,7 +63,7 @@ echo "Function devizovy_trh deployed successfully"
 echo "Deploying function: weather_report"
 call gcloud functions deploy weather_report ^
     --runtime python310 ^
-    --trigger-topic case_study_default_topic ^
+    --trigger-http ^
     --allow-unauthenticated ^
     --source ./CloudFunctions/weather_report/ ^
     --env-vars-file CloudFunctions/weather_report/temp_env.yaml ^
@@ -65,7 +72,27 @@ call gcloud functions deploy weather_report ^
 
 echo "Function weather_report deployed successfully"
 
+echo "Deploying function: public_trips"
+call gcloud functions deploy public_trips ^
+    --runtime python310 ^
+    --trigger-http ^
+    --allow-unauthenticated ^
+    --source ./CloudFunctions/public_trips/ ^
+    --env-vars-file CloudFunctions/public_trips/temp_env.yaml ^
+    --entry-point get_trips_from_public_dataset ^
+    --gen2
+
+echo "Function public_trips deployed successfully"
+
+echo "Deploying workflow"
+call gcloud workflows deploy workflow ^
+    --source ./Workflow/case_study_workflow.yaml ^
+    --location europe-west3
+
+echo "Workflow deployed successfully"
+
 REM Clean up temporary env.yaml files
 del C:\Users\Filip\Documents\case-study-azsde\case-study-azsde\CloudFunctions\blob_storage\temp_env.yaml
 del C:\Users\Filip\Documents\case-study-azsde\case-study-azsde\CloudFunctions\devizovy_trh\temp_env.yaml
 del C:\Users\Filip\Documents\case-study-azsde\case-study-azsde\CloudFunctions\weather_report\temp_env.yaml
+del C:\Users\Filip\Documents\case-study-azsde\case-study-azsde\CloudFunctions\public_trips\temp_env.yaml
